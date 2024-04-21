87e6706ba83d7f92beaf390de9e0bf0e31742802
Maryammmy <malaksameh18112004@gmail.com>
2024-04-21 23:02:52 +0200

    loading

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -24,7 +24,7 @@ export default function Contact() {
 
       </div>
       </div>
-      <div className="container-fulid mx-5 px-5">
+      <div className="container">
       <div className='d-flex justify-content-center py-3'>
         <div className='mx-3'><a href="misrnews" className=' text-decoration-none text-black fw-bolder'>أخبار مصر</a></div>
         <div  className='mx-3'><a href="policy" className=' text-decoration-none text-black fw-bolder'> سياسة</a></div>
