27e3d4f64627509dc26448b498e4799794f4fc20
Maryammmy <malaksameh18112004@gmail.com>
2024-05-01 19:47:53 +0300

    tt

--- src/Components/Media/Media.jsx
+++ src/Components/Media/Media.jsx
@@ -8,6 +8,7 @@ import CardSkeleton from '../CardSkeleton/CardSkeleton';
 const Media = () => {
   const { setSelected } = useContext(storecontext);
   const navigate = useNavigate();
+  const [subItem, setSubItem] = useState('انفوجراف');
   const [articleData, setArticleData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -30,15 +31,22 @@ const Media = () => {
 
   async function fetchDataFromFirestoreByCategory() {
     try {
-      const querySnapshot = await getDocs(query(collection(db, 'Artciles'), where('categoryName', '==', 'مالتي ميديا')));
-
+      let querySnapshot;
+      if (subItem === 'انفوجراف') {
+        querySnapshot = await getDocs(query(collection(db, 'Artciles'), where('subCategory', '==', 'انفوجراف')));
+      } else if (subItem === 'فيديو') {
+        querySnapshot = await getDocs(query(collection(db, 'Artciles'), where('subCategory', '==', 'فيديو')));
+      }
+  
+      const newData = [];
       querySnapshot.forEach((doc) => {
         const data = doc.data();
-       
-        setArticleData(prevState => [...prevState, data]);
+        newData.push(data);
       });
-
+  
+      setArticleData(newData);
       setLoading(false);
+   
     } catch (error) {
       console.error("Error fetching documents:", error);
     }
@@ -46,19 +54,31 @@ const Media = () => {
 
   useEffect(() => {
     fetchDataFromFirestoreByCategory();
-  }, []);
+  }, [subItem]);
 
   function handleTitleClick(item) {
     setSelected(item);
     navigate('/title');
   }
-
+  const handleSubSelectChange = (e) => {
+    setSubItem(e.target.value);
+    console.log(subItem)
+  };
   return (
     <div className=' w-60 bg-white my-3 shadow me-lg-5'>
 
 
       <>
-        <h4 className=' px-2 py-2 shadow brdr-top brdr-bottom fw-bolder'>انفوجراف</h4>
+        
+        <h4 className=' px-2 py-2 shadow brdr-top brdr-bottom fw-bolder'>مالتي ميديا"</h4>
+        <select
+              name="subCategory"
+              value={subItem}
+              onChange={handleSubSelectChange}
+              className='my-2 form-control w-input'>
+              <option value="انفوجراف">انفوجراف</option>
+              <option value="فيديو">فيديو</option>
+            </select>
       {loading?<CardSkeleton cards={10}/> :  <div className='container-fluid'>
 
 {articleData.map((article, index) => (
@@ -69,7 +89,6 @@ const Media = () => {
       </div>
     </div>
     <div className="col-md-7 padding-right pe-xl-4 pe-xxl-0">
-    <h4 className='fw-bolder title pt-3 pt-md-0'>{article.subCategory}</h4>
       <h4 className='fw-bolder title pt-3 pt-md-0'>{article.title}</h4>
       <p className='time'>{formatDate(article.date)}</p>
     </div>
